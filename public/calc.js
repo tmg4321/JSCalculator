cfb95c92c98dbb8c0132f5224f61cb15e1d22801
Miles Grandin <miles.grandin@gmail.com>
2017-06-18 12:01:10 -0600

    calc recognizes keyboard inputs

--- public/calc.js
+++ public/calc.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
   $('.number').on('click',numberToDisplay);
   $('.operator').on('click',operatorToDisplay);
+  $(document).on('keydown',checkKeyStroke);
 });
-var clickClear = function() {
+
+
+var clickClear = function() { // clears display
   $('#display').empty();
 }
-var numberToDisplay = function(e) {
-  var number = $(this).text();
+
+var checkKeyStroke = function(e) {
+  if (e.keyCode >= 48 && e.keyCode <= 58) {
+    numberToDisplay(e.key);
+    return;
+  }
+
+  if (e.key==='+'||e.key==='-'||e.key==='*'||e.key==='/'
+    ||e.key==='='||e.key==='.'||e.key==='Backspace'||e.key==='Enter') {
+    operatorToDisplay(e.key);
+    return;
+  }
+
+}
+var numberToDisplay = function(e) { // displays number based on user click
+  var number = isNaN(e) ? $(this).text(): e;
   var currentDisplay = $('#display').text();
   if (currentDisplay.length===0) {
     $('#display').text(number);
@@ -37,19 +54,21 @@ var displayDecimal = function() {
 }
 
 var operatorToDisplay = function(e) {
-  if ($(this).attr('id') === 'clear') { //if C clicked, clear display and return
+  var operator = (e==='+'||e==='-'||e==='*'
+    ||e==='/'||e==='='||e==='.'||e==='Backspace') ? e : $(this).text();
+  if ($(this).attr('id') === 'clear'|| e==='Backspace') { //if 'C' clicked, clear display and return
     clickClear();
     return;
   }
-  if ($(this).attr('id') === 'equals') { //if = clicked, do the math and return
+  if ($(this).attr('id') === 'equals'||e==='='||e==='Enter') { //if '=' clicked, do the math and return
     doTheMath();
     return;
   }
-  if ($(this).attr('id') === 'decimal') {
+  if ($(this).attr('id') === 'decimal'||e==='.') { // if '.' clicked, displayDecimal
     displayDecimal();
     return;
   }
-  var operator = $(this).text(); // otherwise, add operator to display
+  // var operator = $(this).text(); // otherwise, add operator to display
   var currentDisplay = $('#display').text();
   $('#display').text(currentDisplay+' '+operator);
 }
@@ -59,7 +78,6 @@ var doTheMath = function() {
   var dsplyArry = $('#display').text().split(' '); //split numbers and operators
   var operand1 = dsplyArry[0];
   var operator = dsplyArry[1];
-  console.log(operator);
   var operand2 = dsplyArry[2];
   switch (operator) {
     case '+':
